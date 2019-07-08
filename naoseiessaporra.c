07bb98d3b9af9448deeba3ddab0d4361f252ed1d
Thiago Moraes <tmoraes505@gmail.com>
2019-07-08 17:14:27 -0300

    agora ta bom

--- naoseiessaporra.c
+++ naoseiessaporra.c
@@ -73,14 +73,14 @@ int main() {
     if((tabuleiro[0]==1 && tabuleiro[1]==1 && tabuleiro[2] == 1) || (tabuleiro[3]==1 && tabuleiro[4]==1 && tabuleiro[5] ==1)
         || (tabuleiro[6]==1 && tabuleiro[7]==1 && tabuleiro[8] == 1) || (tabuleiro[0]==1 && tabuleiro[3]==1 && tabuleiro[6] == 1)
             || (tabuleiro[1]==1 && tabuleiro[4]==1 && tabuleiro[7] == 1) || (tabuleiro[2]==1 && tabuleiro[5]==1 && tabuleiro[8] == 1)
-                || (tabuleiro[0]==1 && tabuleiro[4]==1 && tabuleiro[8] == 1) || (tabuleiro[6]==1 && tabuleiro[4]==1 && tabuleiro[2] == 1){
+                || (tabuleiro[0]==1 && tabuleiro[4]==1 && tabuleiro[8] == 1) || (tabuleiro[6]==1 && tabuleiro[4]==1 && tabuleiro[2] == 1)){
         winner = 1;
     }
 
     if((tabuleiro[0]==2 && tabuleiro[1]==2 && tabuleiro[2] == 2) || (tabuleiro[3]==2 && tabuleiro[4]==2 && tabuleiro[5] ==2)
         || (tabuleiro[6]==2 && tabuleiro[7]==2 && tabuleiro[8] == 2) || (tabuleiro[0]==2 && tabuleiro[3]==2 && tabuleiro[6] == 2)
             || (tabuleiro[1]==2 && tabuleiro[4]==2 && tabuleiro[7] == 2) || (tabuleiro[2]==2 && tabuleiro[5]==2 && tabuleiro[8] == 2)
-                || (tabuleiro[0]==2 && tabuleiro[4]==2 && tabuleiro[8] == 2) || (tabuleiro[6]==2 && tabuleiro[4]==2 && tabuleiro[2] == 2){
+                || (tabuleiro[0]==2 && tabuleiro[4]==2 && tabuleiro[8] == 2) || (tabuleiro[6]==2 && tabuleiro[4]==2 && tabuleiro[2] == 2)){
         winner = 2;
     }
 
@@ -111,7 +111,7 @@ int main() {
         }
 
         fseek(arquivo, sizeof(int), SEEK_SET);
-        fwrite(tabuleiro, 9, sizeof(int, arquivo);
+        fwrite(tabuleiro, 9, sizeof(int), arquivo);
     }
 
     // Imprimindo os resultados e leituras do arquivo
@@ -122,18 +122,16 @@ int main() {
             printf("<title>Jogo da velha - CGI + Arquivos</title>");
             printf("<link rel=\"stylesheet\" href=\"../style.css\">");
         printf("</head>");
-        printf("<body class=\"prox%d\">", jogador); // Para o CSS "saber" quem vai jogar e mudar o fundo do placar, aplica-se a classe (prox1 / prox2) � tag body e utiliza-se o seletor ("body.prox1 > a > div#placar > div#o" / "body.prox2 > a > div#placar > div#x")
+        printf("<body class=\"prox%d\">", player); // Para o CSS "saber" quem vai jogar e mudar o fundo do placar, aplica-se a classe (prox1 / prox2) � tag body e utiliza-se o seletor ("body.prox1 > a > div#placar > div#o" / "body.prox2 > a > div#placar > div#x")
             printf("<a href=\"?\">"); // Ao utilizar um link sem definir nenhum endere�o, ele reabrir� o mesmo arquivo. Ao colocar "?", ele abre o mesmo arquivo sem nenhum par�metro de URL
                 printf("<div id=\"placar\">");
                     printf("<div id=\"o\">");
                         printf("<span>O</span>");
                         printf("<br>");
-                        printf("<span>%d</span>", placar[0]); // Imprime o placar de O
                     printf("</div>");
                     printf("<div id=\"x\">");
                         printf("<span>X</span>");
                         printf("<br>");
-                        printf("<span>%d</span>", placar[1]); // Imprime o placar de X
                     printf("</div>");
                 printf("</div>");
             printf("</a>");
@@ -144,17 +142,18 @@ int main() {
                     "cell0", "cell1" e "cell2" servem para o CSS exibir uma imagem
                     no fundo do bloco de quem fez a jogada naquele bloco.
                 */
-                for (i = 0; i < 9; i++) {
+                i = 0;
+                while(i < 9){
 
                     printf("<a");
-                    if (blocos[i] == 0) printf(" href=\"?bloco=%d&proxJogada=%d\"", i+1, jogador); // Se ningu�m tiver jogado, insere a refer�ncia de quais s�o os par�metros para efetivar a jogada naquele bloco
+                    if (tabuleiro[i] == 0) printf(" href=\"?bloco=%d&proxJogada=%d\"", i+1, player); // Se ningu�m tiver jogado, insere a refer�ncia de quais s�o os par�metros para efetivar a jogada naquele bloco
                     printf(">");
-                    printf("<div class=\"cell%d\"></div>", blocos[i]); // Imprime 0 se ningu�m tiver jogado, 1 se a jogada for O e 2 se for X
+                    printf("<div class=\"cell%d\"></div>", tabuleiro[i]); // Imprime 0 se ningu�m tiver jogado, 1 se a jogada for O e 2 se for X
                     printf("</a>");
-
+                    i += 1;
                 }
             printf("</div>");
-            printf("<div id=\"log\">%s</div>", msg);
+            printf("<div id=\"log\">%s</div>", resposta);
         printf("</body>");
     printf("</html>");
 
